5bdbbcca17132e61ec64b547a485c8d195a60792
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2018-06-14 15:12:19 +0200

    Added password validator tests

---
+++ ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/PasswordValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerLibrary2.PasswordValidator
+{
+    [TestFixture]
+    class PasswordValidatorTests
+    {
+        /*
+         * 
+         * A minimum 15 characters
+         * At least 2 digits have to be present
+         * At least 2 upper case characterws have to be present
+         * At least one special character needs to be present
+         * Can not have consecutive characters (same character twice, ie. x22xd)
+         * 
+         * */
+        [Test]
+        public void Test()
+        {
+            var password = "abc";
+            var sut = this.GetValidator();
+            var isPasswordValid = sut.IsPasswordValid(password);
+            Assert.IsFalse(isPasswordValid);
+        }
+
+        private IPasswordValidator GetValidator()
+        {
+            return new MockPasswordValidator();
+        }
+    }
+}
