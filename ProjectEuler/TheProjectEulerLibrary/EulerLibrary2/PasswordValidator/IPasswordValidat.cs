cf70487b8cde2590be1ed3d49568360a9bd35d6f
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2018-06-14 15:03:05 +0200

    Added Shape (inheritance)

---
+++ ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/IPasswordValidat.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace EulerLibrary2.PasswordValidator
+{
+    interface IPasswordValidator
+    {
+        string Name { get; }
+
+        bool IsPasswordValid(string password);
+    }
+}
---
+++ ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/MockPasswordValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EulerLibrary2.PasswordValidator
+{
+    class MockPasswordValidator : IPasswordValidator
+    {
+        public string Name => throw new NotImplementedException();
+
+        public bool IsPasswordValid(string password) => throw new NotImplementedException();
+    }
+}
