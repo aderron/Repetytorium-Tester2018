b195d28c47a05bf9d62907fb45e9b8a9f269dcab
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2018-06-14 11:18:09 +0200

    Added IsPrimeNumber

---
+++ ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/Tools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerLibrary2
+{
+    class Tools
+    {
+        public static bool IsPrimeNumber(int number)
+        {
+            if (number == 1)
+            {
+                return false; // because definition
+            }
+
+            for (var i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
