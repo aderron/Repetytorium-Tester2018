fed9dd50e3521d56589e41aa90a93b9e182403fb
Maciej Bruski <m.globalbot@gmail.com>
2018-06-15 00:40:37 +0200

    AddTests
    AddTestsValidate

--- ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/PasswordValidatorTests.cs
+++ ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/PasswordValidatorTests.cs
@@ -21,9 +21,33 @@ namespace EulerLibrary2.PasswordValidator
         [Test]
         public void IsABCInvalid()
         {
-            var password = "ABCabcabc123#@!";
+            var password = "ABkCabcaoc123kl!";
             var isPasswordValid = this.validator.IsPasswordValid(password);
             Assert.IsTrue(isPasswordValid);
+        }
+
+
+        [TestCase("ABkCabcaoc123kl!",  TestName = "Test true")]
+        [TestCase("ABkCabcaoc123kl!dfdfdfdksksksksksksksksksksksksksksksksksksksksksksksksksksksksksksksksksksksksksksk", TestName = "Test true 100 L")]
+        [TestCase("!@!@!@!@!@12WEWEWEWEWEWdsad", TestName = "Test true 10 * (s+u) + 2 d")]
+        [TestCase("!@!@!@!@!@121212121212WEWEWEWEWEWdsad", TestName = "Test true 10 * (d+s+u)")]
+        public void PasswordTestValidTrue(string password)
+        {
+            Assert.IsTrue(this.validator.IsPasswordValid(password));
+        }
+
+        [TestCase("ABkCabcoc123kl", TestName = "fTest short (14)")]
+        [TestCase("WE@sdsdk@2sdsdsd", TestName = "fTest false")]
+        [TestCase("!@!@!@!@!@12WEWEWEWEWEWdsadd", TestName = "fTest same char")]
+        [TestCase("!@!@!@!@!@11WEWEWEWEWEWdsad", TestName = "fTest same d")]
+        [TestCase("!!@!@!@!@!@11WEWEWEWEWEWdsad", TestName = "fTest same s")]
+        [TestCase("!@!@!@!@!@12WEWEWEWEWEWdsaDD", TestName = "fTest same u")]
+        [TestCase("ABkCabcaocsa3kl!", TestName = "fTest 1*d")]
+        [TestCase("Abkcabcaoc123kl!", TestName = "fTest 1*u")]
+        [TestCase("ABkCabcaoc123kl2", TestName = "fTest no special")]
+        public void PasswordTestValidFalse(string password)
+        {
+            Assert.IsFalse(this.validator.IsPasswordValid(password));
         }
     }
 }
