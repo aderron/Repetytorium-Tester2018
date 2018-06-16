c3bbaccd44123c35bb8dc7b39008bd34c33558cd
Piotr <bugi353@gmail.com>
2018-06-16 13:06:41 +0200

    Zmiany

--- ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/PasswordValidatorTests.cs
+++ ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/PasswordValidatorTests.cs
@@ -22,46 +22,6 @@ namespace EulerLibrary2.PasswordValidator
         [Test]
         public void IsABCInvalid()
         {
-            var password = "ABCabcabc123#@!";
-            var isPasswordValid = this.validator.IsPasswordValid(password);
-            Assert.IsTrue(isPasswordValid);
-        }
-
-        [Test]
-        public void IsExceptionHandling()
-        {
-            var password = "A";
-            var exception = Assert.Throws<ApplicationException>(
-                () => this.validator.IsPasswordValid(password));
-            var expectedMessage = "x";
-            Assert.AreEqual(expectedMessage, exception.Message);
-        }
-    }
-}
-﻿using NUnit.Framework;
-using PasswordValidator;
-using System;
-
-namespace EulerLibrary2.PasswordValidator
-{
-    [TestFixture]
-    class PasswordValidatorTests
-    {
-        /*
-         * 
-         * A minimum 15 characters
-         * At least 2 digits have to be present
-         * At least 2 upper case characterws have to be present
-         * At least one special character needs to be present
-         * Can not have consecutive characters (same character twice, ie. x22xd)
-         * 
-         * */
-
-        private IPasswordValidator validator = new ExternalPasswordValidator();
-
-        [Test]
-        public void IsABCInvalid()
-        {
             var password = "ABCC";
             var isPasswordValid = this.validator.IsPasswordValid(password);
             Assert.IsTrue(isPasswordValid);
