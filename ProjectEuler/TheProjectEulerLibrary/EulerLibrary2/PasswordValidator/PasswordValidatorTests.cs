56766dcb7b0f1713d6e6e56075c8b6bf0b441019
DerAd <a.derron@wp.pl>
2018-06-17 18:18:46 +0200

    Defect tests and report

--- ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/PasswordValidatorTests.cs
+++ ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/PasswordValidatorTests.cs
@@ -17,10 +17,11 @@ namespace EulerLibrary2.PasswordValidator
          * 
          * */
 
+
         private IPasswordValidator validator = new ExternalPasswordValidator();
 
         [Test]
-        public void IsABCInvalid()
+        public void ValidCase()
         {
             var password = "ABCabcabc123#@!";
             var isPasswordValid = this.validator.IsPasswordValid(password);
@@ -28,14 +29,91 @@ namespace EulerLibrary2.PasswordValidator
         }
 
         [Test]
-        public void IsPasswordValid_OneCharacterPassword_ThrowsTooShort()
+        public void TooShortPassword()
         {
-            var tooShortPassword = "A";
-            var expectedMessage = "Password is too short. It needs to be at least 15 characters long";
-            
-            var exception = Assert.Throws<ApplicationException>(
-                () => this.validator.IsPasswordValid(tooShortPassword));
-            Assert.AreEqual(expectedMessage, exception.Message);
+            var password = "ABCabcabc123#@";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
+        }
+
+        [Test]
+        public void MinTwoDigits()
+        {
+            var password = "ABCabcabc3kad!;";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
+        }
+
+        [Test]
+        public void DuplicatedCharacters()
+        {
+            var password = "ABCabcabccac123kad!;";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
+        }
+
+        [Test]
+        public void MinTwoUpperCase()
+        {
+            var password = "Aabcabcac123kad!;";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
+        }
+
+        [Test]
+        public void NoSpecialChar()
+        {
+            var password = "ABabcabcac123kad;";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
+
+        }
+
+        [Test]
+        public void TooManyDigitsPassword()
+        {
+            var password = "ABCabcabcabc123kad!12345632;";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
+        }
+
+        [Test]
+        public void NotSupportedLength21()
+        {
+            var password = "ABCabcabcabc123kad!12";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
+        }
+
+        [Test]
+        public void NotSupportedLength30()
+        {
+            var password = "ABCabcabcabc123kad!1234563235;";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
+        }
+
+        [Test]
+        public void ExceedChar255()
+        {
+            var password = "ACabcabcabc123kadACabcabcabc123kad!1234563235" +
+                "bc123kad!1234563235ACabcabcabc123kad!1234563235ACabcabca" +
+                "bc123kad!1234563235ACabcabcabc123kad!1234563235ACabca" +
+                "bcabc123kad!1234563235!1234563ACabcabcabc123kad!12345632" +
+                "35ACabcabcabc123kad!123ad!1234563235!1ad!1234563235!145" +
+                "63235ACabcabcabc123kad!12345632352ad!1234583235!1" +
+                "ad!123453kad!1234563235ACabcabcabc123kad!1234563235ACabca" +
+                "bcabc123kad!1234563235!1234563ACabcabcabc123kad!12345632";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
+        }
+
+        [Test]
+        public void TooManyUppercaseLetters()
+        {
+            var password = "ABCABCABCABCABCABCABCABCABCABCabcabcabc123kad!;";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
         }
     }
 }
