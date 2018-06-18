2d6bd1db280f453e37dc046d3c1663a0d149eba4
Anabel9222 <Anabel9222@anna.ossowska92@gmail.com>
2018-06-18 22:41:45 +0200

    Added Euler solutions

---
+++ EulerZadania/zad2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fibA = 1;
+            var fibB = 2;
+            long sum = 0;
+
+
+            while (fibB < 4000000)
+            {
+                if (fibB % 2 == 0)
+                {
+                    sum += fibB;
+                }
+
+                var oldA = fibA;
+                fibA = fibB;
+                fibB = oldA + fibA;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EulerZadania/zad3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dzielna = 600851475143;
+            var dzielnik = 2;
+            long najDzielnik = 0;
+            bool znaleziono = false;
+
+
+            while (znaleziono == false)
+            {
+                while (dzielna % dzielnik != 0)
+                {
+                    dzielnik++;
+                }
+                najDzielnik = dzielna / dzielnik;
+                Console.WriteLine("Aktualny najwiekszy dzielnik: " + najDzielnik);
+
+                for (var i = 2; i < najDzielnik; i++)
+                {
+                    if (najDzielnik % i == 0)
+                    {
+                        Console.WriteLine("Aktualny najwiekszy dzielnik nie jest liczba pierwsza, bo dzieli sie przez: " + i);
+                        dzielnik++;
+                        break;
+                    }
+                    else if (i == najDzielnik - 1)
+                    {
+                        znaleziono = true;
+                    }
+                }
+            }
+            Console.WriteLine(najDzielnik + " to najwiekszy dzielnik bedacy liczba pierwsza.");
+            Console.ReadKey();
+        }
+    }
+}
