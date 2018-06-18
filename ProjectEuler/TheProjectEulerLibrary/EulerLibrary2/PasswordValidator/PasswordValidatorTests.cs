80238a08e04489ccfb864e46608f581408f56181
Anabel9222 <Anabel9222@anna.ossowska92@gmail.com>
2018-06-18 22:24:35 +0200

    Added Remaining PasswordValidator class

---
+++ ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/PasswordValidatorTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using PasswordValidator;
+using System;
+
+namespace EulerLibrary2.PasswordValidator
+{
+    [TestFixture]
+    class PasswordValidatorTests
+    {
+        /*
+         * 
+         * A minimum 15 characters
+         * At least 2 digits have to be present
+         * At least 2 upper case characterws have to be present
+         * At least one special character needs to be present
+         * Can not have consecutive characters (same character twice, ie. x22xd)
+         * 
+         * */
+
+        private IPasswordValidator validator = new ExternalPasswordValidator();
+
+        [Test]
+        public void IsABCInvalid()
+        {
+            var password = "ABCabcabc123#@!";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
+        }
+
+        [Test]
+        public void IsPasswordValid_OneCharacterPassword_ThrowsTooShort()
+        {
+            var tooShortPassword = "A";
+            var expectedMessage = "Password is too short. It needs to be at least 15 characters long";
+            
+            var exception = Assert.Throws<ApplicationException>(
+                () => this.validator.IsPasswordValid(tooShortPassword));
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+    }
+}
