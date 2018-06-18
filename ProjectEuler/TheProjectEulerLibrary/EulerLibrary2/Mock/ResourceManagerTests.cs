4dbdbd20bc1c5f8629313ed4bb7fc9d86450e752
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2018-06-18 12:01:10 +0200

    Excersise

--- ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/Mock/ResourceManagerTests.cs
+++ ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/Mock/ResourceManagerTests.cs
@@ -23,7 +23,24 @@ namespace EulerLibrary2.Mock
             // Assert
             connectorMock.Verify(m => m.GetContent(resourceName), Times.Once); // sprawdza czy metoda została zawołana
             connectorMock.Verify(m => m.PostContent(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+            // Verify IEncryption
+        }
 
+        [Test]
+        public void Post_VerifyIfCallsRightMethods()
+        {
+            // Arange
+            var connectorMock = new Mock<IConnector>();
+            var encryptionMock = new Mock<IEncryption>();
+            var key = "XXX";
+            var resourceName = "RES";
+            var content = "Content";
+
+            var x = new ResourceManager(connectorMock.Object, encryptionMock.Object, key);
+
+            // Act
+
+            // Assert
         }
     }
 }
