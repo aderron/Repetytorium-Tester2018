62140fd526c9b53a9b42b784ef88e4256bf77ef9
Unknown <anna.ossowska92@gmail.com>
2018-06-18 23:21:38 +0200

    dodano rozwiazania 2 zadan, spoj

---
+++ SPOJ/ZadFCTRL3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ZadFCTRL3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbe: ");
+            var liczba = uint.Parse(Console.ReadLine());
+            long wynik = 1;
+
+            for(int i=1; i<=liczba; i++)
+            {
+                wynik = wynik * i;
+            }
+
+            long jednosci = wynik % 10;
+            long dziesiatki = (wynik % 100) / 10;
+            Console.WriteLine("jednosci: " + jednosci);
+            Console.WriteLine("dziesiatki: " + dziesiatki);
+            Console.ReadKey();     
+        }
+    }
+}
---
+++ SPOJ/ZadIsPrime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadIsPrime
+{
+    class Program
+    {
+        static String IsPrime(int a)
+        {
+            for(int i=2; i<a; i++)
+            {
+                if(a % i == 0)
+                {
+                    return "NIE";
+                }
+            }
+            return "TAK";
+        }
+        static void Main(string[] args)
+        {
+            int a = 7;
+            String wynik = IsPrime(a);
+            Console.WriteLine("'a' jest liczba pierwsza? -" + wynik);
+            Console.ReadKey();
+        }
+    }
+}
