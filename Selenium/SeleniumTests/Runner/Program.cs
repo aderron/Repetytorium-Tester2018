4b2211fd3e13a634051146f3f0edc7aff5a8f51a
Marcin Konrad Ceglarek <marcin.ceglarek@gmail.com>
2018-06-22 13:10:46 +0200

    Added initial selenium tests

---
+++ Selenium/SeleniumTests/Runner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using SeleniumTests;
+
+namespace Runner
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var x = new ChromeTests();
+            //x.PerformTest();
+            var a = "s1.png";
+            var b = "s2.png";
+
+            var bitmapA = new Bitmap(a);
+            var bitmapB = new Bitmap(b);
+
+            var output = ImageTools.CompareImages(bitmapA, bitmapB);
+
+            output.Save("output.png");
+        }
+    }
+}
---
+++ Selenium/SeleniumTests/SeleniumTests/ImageTools.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace SeleniumTests
+{
+    public static class ImageTools
+    {
+        public static Bitmap CompareImages(Bitmap a, Bitmap b)
+        {
+            var bitmap = new Bitmap(a.Width, a.Height);
+
+            for (var column = 0; column < a.Height; column++)
+            {
+                for (var row = 0; row < a.Width; row++)
+                {
+                    var aPixel = a.GetPixel(row, column);
+                    var bPixel = b.GetPixel(row, column);
+
+                    var outputColor = aPixel == bPixel ? Grayscale(aPixel) : CompareColors(aPixel, bPixel);
+                    bitmap.SetPixel(row, column, outputColor);
+                }
+            }
+
+            return bitmap;
+        }
+
+        public static Color Grayscale(Color a)
+        {
+            var aWeight = (a.R + a.G + a.B) / 3;
+            var lightenColor = (255 - aWeight) / 2 + aWeight;
+            return Color.FromArgb(lightenColor, lightenColor, lightenColor);
+        }
+
+        public static Color CompareColors(Color a, Color b)
+        {
+            var aWeight = a.R + a.G + a.B;
+            var bWeight = b.R + b.G + b.B;
+
+            if (aWeight > bWeight)
+            {
+                return Color.FromArgb(aWeight / 3, 0, 0);
+            }
+
+            return Color.FromArgb(0, bWeight / 3, 0);
+        }
+
+        public static Image ToImage(byte[] byteArrayIn)
+        {
+            var ms = new MemoryStream(byteArrayIn);
+            var returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
