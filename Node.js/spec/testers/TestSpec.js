d8f51dd5a7c32ecf532fe474795b3608e21a156c
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2018-06-27 12:57:35 +0200

    Our own Jasmine tests

---
+++ Node.js/spec/testers/TestSpec.js
@@ -0,0 +1,13 @@
+
+
+describe("A Javascript is stupid because", function() {
+    it("comparing two different arrays return true", function() {
+        expect(new Array() == new Array()).not.toBe(true);
+    });
+
+    it("when evaluating \"0\" == 0 returns true", function() {
+        expect("0"==0).toBe(true)
+    });
+});
+  
+  
