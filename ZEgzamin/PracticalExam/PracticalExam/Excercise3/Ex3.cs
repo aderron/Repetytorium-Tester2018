436383ac60ace979b85523e75b9ba9fb821c3d15
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2018-06-29 10:44:48 +0200

    Updated questions

---
+++ ZEgzamin/PracticalExam/PracticalExam/Excercise3/Ex3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PracticalExam.Excercise3
+{
+    public static class Ex3
+    {
+        /*
+         * Napisz metodę, która odwraca stringa (ze stringa może -> eżom)
+         */
+        public static string Reverse(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
