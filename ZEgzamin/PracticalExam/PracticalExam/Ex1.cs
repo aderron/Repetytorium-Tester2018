bce7da480b0a4a0eabc6469b7b7f26077575d2f1
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2018-06-29 09:47:05 +0200

    Zadania

--- ZEgzamin/PracticalExam/PracticalExam/Ex1.cs
+++ ZEgzamin/PracticalExam/PracticalExam/Ex1.cs
@@ -1,19 +1,52 @@
-﻿using NUnit.Framework;
+﻿using System.Globalization;
+using NUnit.Framework;
+
+/*
+ * Napisz testy, które przetestują każde wywołane metody IsPalindrome (dla longa, doubla i stringa)
+ */
 
 namespace PracticalExam
 {
-    public class Ex1
-    {
-
-    }
-
     [TestFixture]
     public class Tests
     {
         [Test]
         public void X()
         {
+            // Arrange
 
+            // Act
+            //Ex1.IsPalindrome();
+
+            // Assert
         }
     }
+
+    public class Ex1
+    {
+        public static bool IsPalindrome(double a)
+        {
+            return IsPalindrome(a.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public static bool IsPalindrome(long a)
+        {
+            return IsPalindrome(a.ToString());
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            for (var i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+
 }
---
+++ ZEgzamin/PracticalExam/PracticalExam/Ex2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace PracticalExam
+{
+    /*
+     * Praktykant ze stanów zjednoczonych źle wyeksportował plik testwoy (AccountingData.json)
+     * Daty w tym pliku są w złym formacie (amerykańskim a nie europejskim). Popraw plik
+     */
+
+    [TestFixture]
+    public class Ex2Tests
+    {
+        [Test]
+        public void ReadData()
+        {
+            var result = Ex2.ReadData();
+        }
+    }
+
+    public class Ex2
+    {
+        public static List<Row> ReadData()
+        {
+            var data = File.ReadAllText("Ex2Data.json");
+            var rows = JsonConvert.DeserializeObject<List<Row>>(data);
+            return rows;
+        }
+
+        public class Row
+        {
+            public DateTime Date { get; set; }
+            public DateTime AccountingDate { get; set; }
+            public bool IsLogged {get; set; }
+            public string Client { get; set; }
+            public double Amount { get; set; }
+        }
+    }
+}
